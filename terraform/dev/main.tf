ca098ff5d2f95387cccde21875e501f823246b34
bhagya nanditha <67181954+1234209@users.noreply.github.com>
2026-01-31 10:33:32 +0530

    Create main.tf

---
+++ terraform/dev/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2" {
+  source         = "../modules/ec2"
+  ami            = var.ami
+  instance_type  = var.instance_type
+  env            = "dev"
+}
